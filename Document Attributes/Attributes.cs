057cd15c89513fd76d246e95ec2403e3a07d8904
jaguar48 <okonkwostanley67@yahoo.com>
2023-01-17 17:37:34 +0100

    Add project files.

---
+++ Document Attributes/Attributes.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection;
+
+namespace DocumentAttribute
+{
+
+
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true),]
+    public class DocumentAttribute : Attribute
+    {
+        [Document("This makes a get and set for the description")]
+        public string Description { get; set; }
+        [Document("This makes a get and set for the input")]
+        public string Input { get; set; }
+        [Document("This makes a get and set for the output")]
+        public string Output { get; set; }
+
+        [Document("This is a sample constructor that takes only one parameter", "takes description as input")]
+        public DocumentAttribute(string description, string input = "", string output = "")
+        {
+            Input = input;
+            Output = output;
+            Description = description;
+        }
+    }
+
+    [Document("This is a sample class", " sample input parameters")]
+    public class SampleClass
+    {
+        [Document("This is a sample method", "input parameters", "output parameters")]
+        public string SampleMethod(string name)
+        {
+            return name + " is a student of " + SchoolEnum.Brighgirls;
+
+        }
+
+    }
+    [Document("This contains enumerable for schools")]
+    public enum SchoolEnum
+    {
+        Highschool,
+        Brightdays,
+        Brighgirls
+    }
+
+}
---
+++ Document Attributes/Program.cs
@@ -0,0 +1,83 @@
+﻿
+using System.Reflection;
+
+namespace DocumentAttribute
+{
+
+    internal class Program
+    {
+
+        public static void GetMethods(Type type)
+        {
+
+
+            MethodInfo[] getmeth = type.GetMethods();
+            for (int i = 0; i < getmeth.GetLength(0); i++)
+            {
+                object[] methinfo = getmeth[i].GetCustomAttributes(true);
+
+                foreach (Attribute doc in methinfo.Cast<Attribute>())
+                {
+                    if (doc is DocumentAttribute)
+                    {
+                        DocumentAttribute attribute = (DocumentAttribute)doc;
+
+                        Console.WriteLine(attribute.Description);
+                    }
+                }
+            }
+        }
+        public static void GetClass(Type type)
+        {
+            Console.WriteLine("Assembly: {0}", Assembly.GetExecutingAssembly());
+            object[] classinfo = type.GetCustomAttributes(true);
+            foreach (object attr in classinfo)
+            {
+                if (attr is DocumentAttribute)
+                {
+                    DocumentAttribute attribute = (DocumentAttribute)attr;
+
+                    Console.WriteLine($"{attribute.Description} {attribute.Input}");
+                }
+            }
+
+        }
+        public static void GetProps(Type type)
+        {
+            PropertyInfo[] propinfo = type.GetProperties();
+            for (int i = 0; i < propinfo.Length; i++)
+            {
+                object[] prop = propinfo[i].GetCustomAttributes(true);
+
+                foreach (Attribute myprop in prop.Cast<Attribute>())
+                {
+                    if (myprop is DocumentAttribute)
+                    {
+                        DocumentAttribute attribute = (DocumentAttribute)myprop;
+
+                        Console.WriteLine(attribute.Description);
+                    }
+                }
+
+            }
+
+        }
+        public static void GetDocs(Type type)
+        {
+            GetClass(type);
+            GetMethods(type);
+            GetProps(type);
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Program.GetDocs(typeof(SchoolEnum));
+
+            SampleClass sam = new SampleClass();
+
+            Console.WriteLine(sam.SampleMethod("stanley"));
+        }
+    }
+}
